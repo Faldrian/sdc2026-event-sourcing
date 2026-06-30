505257de18e7775edd66f76cc7da5f410d46a4f2
Timo Scheffler <timo.scheffler@seitenbau.com>
2026-06-30 10:41:47 +0200

    init

---
+++ src/main/java/com/seitenbau/sdc/sdc2026eventsourcing/Sdc2026EventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.seitenbau.sdc.sdc2026eventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdc2026EventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdc2026EventSourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seitenbau/sdc/sdc2026eventsourcing/Sdc2026EventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seitenbau.sdc.sdc2026eventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdc2026EventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
